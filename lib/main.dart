005fe2bafc39562c3c32b08e791bdea3c243eee0
mygorn <zeroroll@gmail.com>
2023-08-18 17:50:55 +0700

    Update main.dart
    on web

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo New edit 3',
+      title: 'Flutter Demo New edit 4 On web',
       theme: ThemeData(
         // This is the theme of your application.
         //
